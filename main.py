f80aa0a30f7a046f669c5296eab1df807bde0286
Uzair Rafi <112304028+uzrafi@users.noreply.github.com>
2022-12-19 19:19:17 +0000

    Created bot functionality

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents);
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
+
